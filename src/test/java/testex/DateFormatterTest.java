2a17877da39d9150a25ce658672913afe6acd9c4
Philip West <pwestdk@gmail.com>
2018-03-20 14:48:38 +0100

    asd2

--- src/test/java/testex/DateFormatterTest.java
+++ src/test/java/testex/DateFormatterTest.java
@@ -4,8 +4,6 @@ import java.util.Date;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import static org.mockito.Mockito.when;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
